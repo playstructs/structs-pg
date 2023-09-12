44ec5cbf161e45244378f329979781da576dbf1f
abstrct <abstrct@slow.ninja>
2023-09-12 10:39:51 -0400

    Updated wording of controller within allocation to match CE

--- deploy/table-allocation.sql
+++ deploy/table-allocation.sql
@@ -10,7 +10,7 @@ CREATE TABLE structs.allocation (
 	source_struct_id INTEGER,
 	source_substation_id INTEGER,
 	destination_id INTEGER, 
-	owner CHARACTER VARYING, 
+	controller CHARACTER VARYING,
 	status CHARACTER VARYING, 
 	created_at TIMESTAMPTZ NOT NULL, 
 	updated_at	TIMESTAMPTZ NOT NULL  
