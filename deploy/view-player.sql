58d024076545f2d3f0ef514c1ee6c4faa268dcc5
abstrct <abstrct@slow.ninja>
2024-06-13 16:51:46 -0400

    Fixed a bug in player view

--- deploy/view-player.sql
+++ deploy/view-player.sql
@@ -13,9 +13,9 @@ CREATE OR REPLACE VIEW view.player AS
             COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='3-' || player.id),0) as load,
             COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='4-' || player.id),0) as structs_load,
             COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='2-' || player.id),0) as capacity,
-            COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='2-' || player.substation_id),0) / COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='7-' || player.substation_id),1) as connection_capacity,
+            COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='6-' || player.substation_id),0) as connection_capacity,
             COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='3-' || player.id),0) + COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='4-' || player.id),0) as total_load,
-            COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='2-' || player.id),0) + (COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='2-' || player.substation_id),0) / COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='7-' || player.substation_id),1)) as total_capacity,
+            COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='2-' || player.id),0) + COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='6-' || player.substation_id),0)  as total_capacity,
             primary_address,
             created_at,
             updated_at
