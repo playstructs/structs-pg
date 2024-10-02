16c362ae16f47f9a22f41c917bd744d4b7e8f583
abstrct <abstrct@slow.ninja>
2024-10-02 11:33:57 -0400

    Improved the permission table and added permission views to make the process actually usable on the dev side.

--- deploy/table-permission.sql
+++ deploy/table-permission.sql
@@ -4,6 +4,9 @@ BEGIN;
 
 CREATE UNLOGGED TABLE structs.permission (
    id           CHARACTER VARYING PRIMARY KEY,
+   object_type  INTEGER,
+   object_index CHARACTER VARYING,
+   player_id    CHARACTER VARYING,
    val          INTEGER,
    updated_at	TIMESTAMPTZ NOT NULL
 );
--- deploy/view-player.sql
+++ deploy/view-player.sql
@@ -21,5 +21,58 @@ CREATE OR REPLACE VIEW view.player AS
             updated_at
         FROM structs.player;
 
+
+/*
+ const (
+    // 1
+	PermissionPlay Permission = 1 << iota
+	// 2
+	PermissionUpdate
+	// 4
+	PermissionDelete
+	// 8
+	PermissionAssets
+	// 16
+	PermissionAssociations
+	// 32
+	PermissionGrid
+	// 64
+	Permissions
+)
+ */
+
+CREATE OR REPLACE VIEW view.permission_address AS
+    SELECT
+        permission.object_index as address,
+        (permission.val & 1) > 0 as permission_play,
+        (permission.val & 2) > 0 as permission_update,
+        (permission.val & 4) > 0 as permission_delete,
+        (permission.val & 8) > 0 as permission_assets,
+        (permission.val & 16) > 0 as permission_associations,
+        (permission.val & 32) > 0 as permission_grid,
+        (permission.val & 64) > 0 as permissions,
+        permission.updated_at
+
+    FROM structs.permission
+    WHERE permission.object_type = 0;
+
+CREATE OR REPLACE VIEW view.permission_address AS
+SELECT
+        permission.object_type || '-' || permission.object_index as object_id,
+        permission.player_id as player_id,
+
+        (permission.val & 1) > 0 as permission_play,
+        (permission.val & 2) > 0 as permission_update,
+        (permission.val & 4) > 0 as permission_delete,
+        (permission.val & 8) > 0 as permission_assets,
+        (permission.val & 16) > 0 as permission_associations,
+        (permission.val & 32) > 0 as permission_grid,
+        (permission.val & 64) > 0 as permissions,
+        permission.updated_at
+
+FROM structs.permission
+WHERE permission.object_type != 0;
+
+
 COMMIT;
 
