e14ecf1d354cd638a9fc1d161b2d46c89f693cf0
abstrct <abstrct@slow.ninja>
2024-10-22 14:55:59 -0400

    scratch pad code accidently included

--- deploy/table-stat.sql
+++ deploy/table-stat.sql
@@ -125,9 +125,3 @@ BEGIN;
 COMMIT;
 
 
-SELECT object_index, object_type, time_bucket('5 minutes', time) AS interval,
-       last(value, time)
-FROM stat_capacity
-
-GROUP BY object_index,object_type,  interval
-    ORDER BY interval DESC;
