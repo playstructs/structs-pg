5f454670391459ab44d3965bf72df2a3e91bcd6d
abstrct <abstrct@slow.ninja>
2024-11-27 10:40:19 -0500

    Added some error handling for overly large payloads in planet_activity. It should only happen in extremely complicated attacks.

--- deploy/table-planet.sql
+++ deploy/table-planet.sql
@@ -105,12 +105,13 @@ BEGIN;
     DECLARE
         payload TEXT;
     BEGIN
-        payload := to_jsonb(NEW)::TEXT;
+        payload := (to_jsonb(NEW) || jsonb_build_object('channel','structs.planet.' || NEW.planet_id))::TEXT;
 
         -- Notify payload is max 8000bytes.
         -- Create a smaller stub if the payload is larger
         IF length(payload) > 7995 THEN
             payload := jsonb_build_object(
+                            'channel','structs.planet.' || NEW.planet_id,
                             'planet_id', NEW.planet_id,
                             'seq', NEW.seq,
                             'category', NEW.category,
