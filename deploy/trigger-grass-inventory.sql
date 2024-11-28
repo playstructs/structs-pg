2f974b517c0b03c72e8b49d28f1100acf3f31547
abstrct <abstrct@slow.ninja>
2024-11-28 08:42:26 -0500

    Added more grass

--- deploy/trigger-grass-inventory.sql
+++ deploy/trigger-grass-inventory.sql
@@ -6,8 +6,22 @@ BEGIN;
     $BODY$
     DECLARE
         payload TEXT;
+        subject TEXT;
+
+        guild_id CHARACTER VARYING;
+        player_id CHARACTER VARYING;
     BEGIN
-        payload := (to_jsonb(NEW) || jsonb_build_object('subject','structs.inventory.' || NEW.denom || '.' || NEW.object_id, 'category', NEW.action))::TEXT;
+
+        SELECT player_address.guild_id, player_address.player_id INTO guild_id, player_id FROM structs.player_address where player_address.address = NEW.address;
+
+        subject := 'structs.inventory.'
+                        || NEW.denom || '.'
+                        || (CASE guild_id WHEN '' THEN 'noGuild' ELSE COALESCE(guild_id, 'noGuild') END)::TEXT || '.'
+                        || (CASE player_id WHEN '' THEN 'noPlayer' ELSE COALESCE(player_id, 'noPlayer') END)::TEXT || '.'
+                        || NEW.address;
+
+
+        payload := (to_jsonb(NEW) || jsonb_build_object('subject', subject, 'category', NEW.action))::TEXT;
 
         PERFORM pg_notify('grass', payload);
 
