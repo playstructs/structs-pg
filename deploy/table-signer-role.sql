cd88e0434d8fa15eb657f45e3284e9adb79e56b6
abstrct <abstrct@slow.ninja>
2025-01-11 00:07:44 -0500

    changes to signer roles

--- deploy/table-signer-role.sql
+++ deploy/table-signer-role.sql
@@ -6,15 +6,15 @@ BEGIN;
         'stub',
         'generating',
         'pending',
-        'available',
-        'signing'
+        'ready'
     );
 
     CREATE TABLE signer.role (
         id SERIAL PRIMARY KEY,
         player_id CHARACTER VARYING,
+        guild_id CHARACTER VARYING,
         status structs.signer_role_status,
-
+        system_only BOOLEAN DEFAULT false,
         created_at TIMESTAMPTZ DEFAULT NOW(),
         updated_at	TIMESTAMPTZ DEFAULT NOW()
     );
