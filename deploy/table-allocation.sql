6e968781ced250d176c3a9c2e130a16b66865427
abstrct <abstrct@slow.ninja>
2025-04-04 11:00:46 -0400

    Whoops, removing some sketchpad sql

--- deploy/table-allocation.sql
+++ deploy/table-allocation.sql
@@ -2,30 +2,21 @@
 
 BEGIN;
 
-CREATE TABLE structs.allocation (
-	id CHARACTER VARYING PRIMARY KEY,
+    CREATE TABLE structs.allocation (
+        id CHARACTER VARYING PRIMARY KEY,
 
-    allocation_type CHARACTER VARYING,
+        allocation_type CHARACTER VARYING,
 
-	source_id CHARACTER VARYING,
-    index INTEGER,
-	destination_id CHARACTER VARYING,
+        source_id CHARACTER VARYING,
+        index INTEGER,
+        destination_id CHARACTER VARYING,
 
-    creator CHARACTER VARYING,
-    controller CHARACTER VARYING,
-    locked boolean,
+        creator CHARACTER VARYING,
+        controller CHARACTER VARYING,
+        locked boolean,
 
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-	updated_at	TIMESTAMPTZ DEFAULT NOW()
-); 
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at	TIMESTAMPTZ DEFAULT NOW()
+    );
 
 COMMIT;
-
-
-select id,
-       allocation_type,
-       source_id,
-       destination_id,
-       controller,
-       structs.UNIT_DISPLAY_FORMAT(COALESCE((SELECT grid.val FROM structs.grid WHERE grid.id='5-' || allocation.id),0),'milliwatt') as capacity,
-from structs.allocation;
