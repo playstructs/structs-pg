898a67b2ac74d99b46fab4280d5c67f50a1f03d0
abstrct <abstrct@slow.ninja>
2025-04-04 10:59:32 -0400

    More code for service providers

--- deploy/table-player.sql
+++ deploy/table-player.sql
@@ -2,91 +2,91 @@
 
 BEGIN;
 
-CREATE TABLE structs.player (
-	id CHARACTER VARYING PRIMARY KEY,
-    index INTEGER,
+    CREATE TABLE structs.player (
+        id CHARACTER VARYING PRIMARY KEY,
+        index INTEGER,
 
-    creator CHARACTER VARYING,
-    primary_address CHARACTER VARYING,
+        creator CHARACTER VARYING,
+        primary_address CHARACTER VARYING,
 
-    guild_id CHARACTER VARYING,
-	substation_id CHARACTER VARYING,
-	planet_id CHARACTER VARYING,
-    fleet_id CHARACTER VARYING,
+        guild_id CHARACTER VARYING,
+        substation_id CHARACTER VARYING,
+        planet_id CHARACTER VARYING,
+        fleet_id CHARACTER VARYING,
 
-	created_at TIMESTAMPTZ DEFAULT NOW(),
-	updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE TABLE structs.player_meta (
-    id CHARACTER VARYING,
-    guild_id CHARACTER VARYING,
+    CREATE TABLE structs.player_meta (
+        id CHARACTER VARYING,
+        guild_id CHARACTER VARYING,
 
-    username CHARACTER VARYING,
-    pfp CHARACTER VARYING,
+        username CHARACTER VARYING,
+        pfp CHARACTER VARYING,
 
-    status CHARACTER VARYING,
+        status CHARACTER VARYING,
 
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW(),
-    PRIMARY KEY (id, guild_id)
-);
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW(),
+        PRIMARY KEY (id, guild_id)
+    );
 
-CREATE TABLE structs.player_pending (
-    primary_address CHARACTER VARYING PRIMARY KEY,
-    guild_id CHARACTER VARYING,
-    signature CHARACTER VARYING,
-    pubkey CHARACTER VARYING,
-    username CHARACTER VARYING,
-    pfp CHARACTER VARYING,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+    CREATE TABLE structs.player_pending (
+        primary_address CHARACTER VARYING PRIMARY KEY,
+        guild_id CHARACTER VARYING,
+        signature CHARACTER VARYING,
+        pubkey CHARACTER VARYING,
+        username CHARACTER VARYING,
+        pfp CHARACTER VARYING,
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE TABLE structs.player_internal_pending (
-    username CHARACTER VARYING PRIMARY KEY,
-    guild_id CHARACTER VARYING,
-    pfp CHARACTER VARYING,
-    primary_address CHARACTER VARYING,
-    role_id INTEGER,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+    CREATE TABLE structs.player_internal_pending (
+        username CHARACTER VARYING PRIMARY KEY,
+        guild_id CHARACTER VARYING,
+        pfp CHARACTER VARYING,
+        primary_address CHARACTER VARYING,
+        role_id INTEGER,
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE TABLE structs.player_address (
-    address CHARACTER VARYING PRIMARY KEY,
-    player_id CHARACTER VARYING,
-    guild_id CHARACTER VARYING,
-    status CHARACTER VARYING,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+    CREATE TABLE structs.player_address (
+        address CHARACTER VARYING PRIMARY KEY,
+        player_id CHARACTER VARYING,
+        guild_id CHARACTER VARYING,
+        status CHARACTER VARYING,
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE TABLE structs.player_address_activity (
-    address CHARACTER VARYING PRIMARY KEY,
-    block_height BIGINT,
-    block_time TIMESTAMPTZ DEFAULT NOW()
-);
+    CREATE TABLE structs.player_address_activity (
+        address CHARACTER VARYING PRIMARY KEY,
+        block_height BIGINT,
+        block_time TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE TABLE structs.player_address_meta (
-    address CHARACTER VARYING PRIMARY KEY,
-    ip INET,
-    user_agent CHARACTER VARYING,
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+    CREATE TABLE structs.player_address_meta (
+        address CHARACTER VARYING PRIMARY KEY,
+        ip INET,
+        user_agent CHARACTER VARYING,
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE TABLE structs.player_address_pending (
-    address CHARACTER VARYING PRIMARY KEY,
-    signature CHARACTER VARYING,
-    pubkey CHARACTER VARYING,
-    code CHARACTER VARYING NOT NULL DEFAULT structs.unique_human_random(5, 'player_address_pending', 'code'), -- char(5)
-    ip INET,
-    user_agent CHARACTER VARYING,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+    CREATE TABLE structs.player_address_pending (
+        address CHARACTER VARYING PRIMARY KEY,
+        signature CHARACTER VARYING,
+        pubkey CHARACTER VARYING,
+        code CHARACTER VARYING NOT NULL DEFAULT structs.unique_human_random(5, 'player_address_pending', 'code'), -- char(5)
+        ip INET,
+        user_agent CHARACTER VARYING,
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
 
-CREATE UNIQUE INDEX player_address_pending_code_idx ON structs.player_address_pending (code);
+    CREATE UNIQUE INDEX player_address_pending_code_idx ON structs.player_address_pending (code);
 
 
     CREATE TABLE structs.player_external_pending (
