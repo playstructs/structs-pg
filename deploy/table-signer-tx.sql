2cadcd9ef32d337eba63efbeb532387e1f1f172d
abstrct <abstrct@slow.ninja>
2025-04-11 13:12:20 -0400

    Improved the tx_bank_send function to allow more flexibility

--- deploy/table-signer-tx.sql
+++ deploy/table-signer-tx.sql
@@ -208,12 +208,23 @@ BEGIN;
     DECLARE
         _primary_address_sender CHARACTER VARYING;
         _primary_address_recipient CHARACTER VARYING;
+
+        _real_denom CHARACTER VARYING;
+        _real_amount NUMERIC;
     BEGIN
 
         SELECT player.primary_address INTO _primary_address_sender FROM structs.player where player.id = _player_id;
         SELECT player.primary_address INTO _primary_address_recipient FROM structs.player where player.id = _destination_player_id;
 
-        PERFORM signer.CREATE_TRANSACTION(_player_id,0,'bank','send',jsonb_build_array(_primary_address_sender,_primary_address_sender, _amount || _denom),'{}');
+        IF _denom ILIKE 'u%' THEN
+            _real_denom := _denom;
+            _real_amount := _amount;
+        ELSE
+            _real_denom := 'u' || _denom;
+            _real_amount := _amount * 10^6;
+        END IF;
+
+        PERFORM signer.CREATE_TRANSACTION(_player_id,0,'bank','send',jsonb_build_array(_primary_address_sender,_primary_address_sender, _real_amount || _real_denom),'{}');
     END
     $BODY$
     LANGUAGE plpgsql VOLATILE SECURITY DEFINER COST 100;
