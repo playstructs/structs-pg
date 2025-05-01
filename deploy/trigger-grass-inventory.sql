fd02f99270f64ed96737971e0735d2d728606157
abstrct <abstrct@slow.ninja>
2025-04-30 20:37:51 -0400

    Expanding the grass inventory payload

--- deploy/trigger-grass-inventory.sql
+++ deploy/trigger-grass-inventory.sql
@@ -5,33 +5,41 @@ BEGIN;
     CREATE OR REPLACE FUNCTION structs.INVENTORY_NOTIFY() RETURNS trigger AS
     $BODY$
     DECLARE
-        payload TEXT;
-        subject TEXT;
+        _payload TEXT;
+        _subject TEXT;
 
-        guild_id CHARACTER VARYING;
-        player_id CHARACTER VARYING;
+        _guild_id CHARACTER VARYING;
+        _player_id CHARACTER VARYING;
+        _counterparty_player_id CHARACTER VARYING;
+        _counterparty_guild_id CHARACTER VARYING;
     BEGIN
 
-        SELECT player_address.guild_id, player_address.player_id INTO guild_id, player_id FROM structs.player_address where player_address.address = NEW.address;
+        SELECT player_address.guild_id, player_address.player_id INTO _guild_id, _player_id FROM structs.player_address where player_address.address = NEW.address;
+        SELECT player_address.guild_id, player_address.player_id INTO _counterparty_guild_id, _counterparty_player_id FROM structs.player_address where player_address.address = NEW.counterparty;
 
-        guild_id := (CASE guild_id WHEN '' THEN 'noGuild' ELSE COALESCE(guild_id, 'noGuild') END);
-        player_id := (CASE player_id WHEN '' THEN 'noPlayer' ELSE COALESCE(player_id, 'noPlayer') END);
+        _guild_id := (CASE _guild_id WHEN '' THEN 'noGuild' ELSE COALESCE(_guild_id, 'noGuild') END);
+        _player_id := (CASE _player_id WHEN '' THEN 'noPlayer' ELSE COALESCE(_player_id, 'noPlayer') END);
 
-        subject := 'structs.inventory.'
+        _counterparty_player_id := (CASE _player_id WHEN '' THEN 'noPlayer' ELSE COALESCE(_player_id, 'noPlayer') END);
+        _counterparty_guild_id := (CASE _counterparty_guild_id WHEN '' THEN 'noGuild' ELSE COALESCE(_counterparty_guild_id, 'noGuild') END);
+
+        _subject := 'structs.inventory.'
                         || NEW.denom || '.'
                         || guild_id::TEXT || '.'
                         || player_id::TEXT || '.'
                         || NEW.address;
 
 
-        payload := (to_jsonb(NEW)
+        _payload := (to_jsonb(NEW)
                         || jsonb_build_object(
-                            'subject', subject,
+                            'subject', _subject,
                             'category', NEW.action,
-                            'player_id', player_id,
-                            'guild_id', guild_id))::TEXT;
+                            'player_id', _player_id,
+                            'counterparty_player_id', _counterparty_player_id,
+                            'counterparty_guild_id', _counterparty_guild_id,
+                            'guild_id', _guild_id))::TEXT;
 
-        PERFORM pg_notify('grass', payload);
+        PERFORM pg_notify('grass', _payload);
 
         RETURN NEW;
     END
