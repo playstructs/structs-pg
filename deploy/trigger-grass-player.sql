d9d3268ded9da020d5b0c61e40ae52445f68efdf
abstrct <abstrct@slow.ninja>
2025-05-08 09:37:55 -0400

    Added a grass notification for player_address updates

--- deploy/trigger-grass-player.sql
+++ deploy/trigger-grass-player.sql
@@ -59,4 +59,22 @@ BEGIN;
     CREATE TRIGGER PLAYER_META_NOTIFY AFTER INSERT OR UPDATE ON structs.player_meta
         FOR EACH ROW EXECUTE PROCEDURE structs.PLAYER_META_NOTIFY();
 
+
+    CREATE OR REPLACE FUNCTION structs.PLAYER_ADDRESS_NOTIFY() RETURNS trigger AS
+    $BODY$
+    DECLARE
+        payload TEXT;
+    BEGIN
+        payload := (to_jsonb(NEW) || jsonb_build_object('subject','structs.player.' || NEW.guild_id || '.' || NEW.id, 'category', 'player_address'))::TEXT;
+
+        PERFORM pg_notify('grass', payload);
+
+        RETURN NEW;
+    END
+    $BODY$
+        LANGUAGE plpgsql VOLATILE SECURITY DEFINER COST 100;
+
+    CREATE TRIGGER PLAYER_ADDRESS_NOTIFY AFTER INSERT OR UPDATE ON structs.player_address
+        FOR EACH ROW EXECUTE PROCEDURE structs.PLAYER_ADDRESS_NOTIFY();
+
 COMMIT;
