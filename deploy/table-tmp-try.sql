ce8fd0b3a5447018bcbb77c7e7deae0e3c2ec412
abstrct <abstrct@slow.ninja>
2025-08-13 16:37:04 -0400

    Removing my tmp migrate try

--- deploy/table-tmp-try.sql
+++
@@ -1,9 +0,0 @@
--- Deploy structs-pg:table-tmp-try to pg
-
-BEGIN;
-
-CREATE TABLE structs.tmp_try (
-	id CHARACTER VARYING PRIMARY KEY
-);
-
-COMMIT;
--- verify/table-tmp-try.sql
+++
@@ -1,7 +0,0 @@
--- Verify structs-pg:table-tmp-try on pg
-
-BEGIN;
-
--- XXX Add verifications here.
-
-ROLLBACK;
