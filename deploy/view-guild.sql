2f73b13e7f795c0be057d08c9e04800002f37e12
abstrct <abstrct@slow.ninja>
2025-11-02 22:47:22 -0500

    Fixed guild_bank references

--- deploy/view-guild.sql
+++ deploy/view-guild.sql
@@ -46,21 +46,21 @@ BEGIN;
     GROUP BY player_address.guild_id, address_inventory.denom;
 
     CREATE OR REPLACE VIEW view.guild_bank AS
-        WITH base AS (
-            SELECT
-                id,
-                denom,
-                (SELECT sum(case when ledger.direction = 'debit' then ledger.amount_p * -1 ELSE ledger.amount_p END) as balance
-                 FROM structs.ledger
-                 WHERE action in ('minted', 'burned')
-                   and ledger.address = guild_bank.mint_address
-                ) as minted_supply,
-                (SELECT sum(case when ledger.direction='debit' then ledger.amount_p*-1 ELSE ledger.amount_p END) as balance
-                 FROM structs.ledger where ledger.address = guild_bank.collateral_address
-                ) as collateral_balance
-            FROM
-                structs.guild_bank
-        ) SELECT base.*, base.collateral_balance / base.minted_supply as ratio FROM base;
+        WITH list AS (SELECT address_tag.address FROM structs.address_tag WHERE address_tag.label = 'Type' and address_tag.entry = 'Bank Collateral Pool'),
+            base AS (
+                SELECT
+                    addres_tag.entry as guild_id,
+                    'uguild.' || address_tag.entry as denom,
+                    (SELECT SUM(case when ledger.direction = 'debit' then ledger.amount_p * -1 ELSE ledger.amount_p END) as balance
+                     FROM structs.ledger
+                     WHERE action IN ('minted', 'burned') AND ledger.denom = 'uguild.' || address_tag.entry
+                    ) as minted_supply,
+                    (SELECT SUM(case when ledger.direction='debit' then ledger.amount_p*-1 ELSE ledger.amount_p END) as balance
+                     FROM structs.ledger where ledger.address = list.address
+                    ) as collateral_balance
+                FROM list
+                         LEFT JOIN structs.address_tag on address_tag.address = list.address AND address_tag.label = 'GuildId'
+            ) SELECT base.*, base.collateral_balance / base.minted_supply as ratio FROM base;
 
 
     CREATE OR REPLACE VIEW view.leaderboard_guild AS
