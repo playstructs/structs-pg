8cb0ee74016d76647b3c4403edd57ead92cbfdc4
abstrct <abstrct@slow.ninja>
2025-11-17 23:24:32 -0500

    Fixed bug in fleet updates that missed command_struct updates.

---
+++ revert/cache-trigger-add-queue-20251117-fix-fleet-update-handling.sql
@@ -0,0 +1,7 @@
+-- Revert structs-pg:cache-trigger-add-queue-20251117-fix-fleet-update-handling from pg
+
+BEGIN;
+
+ -- Nothing needed
+
+COMMIT;
---
+++ verify/cache-trigger-add-queue-20251117-fix-fleet-update-handling.sql
@@ -0,0 +1,7 @@
+-- Verify structs-pg:cache-trigger-add-queue-20251117-fix-fleet-update-handling on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
