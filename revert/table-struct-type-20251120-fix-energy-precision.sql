23ec34071eb07460382babececefbd0392638f40
abstrct <abstrct@slow.ninja>
2025-11-20 15:46:05 -0500

    Was converting precision with the wrong unit. Fixed now

---
+++ revert/table-struct-type-20251120-fix-energy-precision.sql
@@ -0,0 +1,7 @@
+-- Revert structs-pg:table-struct-type-20251120-fix-energy-precision from pg
+
+BEGIN;
+
+  --
+
+COMMIT;
---
+++ verify/table-struct-type-20251120-fix-energy-precision.sql
@@ -0,0 +1,7 @@
+-- Verify structs-pg:table-struct-type-20251120-fix-energy-precision on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
