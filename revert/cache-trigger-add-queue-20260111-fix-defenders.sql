ebdfa197f22a1eeaf7e6d10e202dc86077818c81
abstrct <abstrct@slow.ninja>
2026-01-11 17:49:37 -0500

    Fixing an issue with defenders

---
+++ revert/cache-trigger-add-queue-20260111-fix-defenders.sql
@@ -0,0 +1,7 @@
+-- Revert structs-pg:cache-trigger-add-queue-20260111-fix-defenders from pg
+
+BEGIN;
+
+
+
+COMMIT;
---
+++ verify/cache-trigger-add-queue-20260111-fix-defenders.sql
@@ -0,0 +1,7 @@
+-- Verify structs-pg:cache-trigger-add-queue-20260111-fix-defenders on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
