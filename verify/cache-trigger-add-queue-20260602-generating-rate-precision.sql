f1428117709ecc8149a72fdb3d451d99de2bfbf7
abstrct <abstrct@slow.ninja>
2026-06-02 13:27:17 -0400

    fix generating_rate on struct_type

---
+++ verify/cache-trigger-add-queue-20260602-generating-rate-precision.sql
@@ -0,0 +1,7 @@
+-- Verify structs-pg:cache-trigger-add-queue-20260602-generating-rate-precision on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
---
+++ verify/table-struct-type-20260602-add-generating-rate-precision.sql
@@ -0,0 +1,7 @@
+-- Verify structs-pg:table-struct-type-20260602-add-generating-rate-precision on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
